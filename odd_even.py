b3b39a4a44b51d74bf52389506b690772a7fbfe9
BalrajeGaikwad <balrajegaikwad55@gmail.com>
2025-01-04 00:02:10 +0530

    find even and add number

---
+++ odd_even.py
@@ -0,0 +1,8 @@
+#(b) Any integer is input through the keyboard. Write a program to find out whether it is an odd number or even number.
+
+
+a=int(input("Enter the number :- "))
+if(a%2==0):
+    print("even number")
+else:
+    print("odd number")
